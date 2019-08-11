5aefbff61b325d44db8cf95b1a06d2e601bb3efc
navdeep-singh-ghotra <NavdeepSingh.Ghotra@gmail.com>
2019-08-11 11:26:24 -0400

    Adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var jsFiles = ['*.js', 'src/**/*.js' ];
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+
+
+gulp.task('style', function(){
+    return gulp.src(jsFiles)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish',{
+        verbose:true
+    }))
+    .pipe(jscs());
+})
